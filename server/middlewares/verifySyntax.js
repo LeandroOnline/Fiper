f3773d096cd865184af2964c10a4c30aaeb3daf7
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-05-03 11:38:22 -0300

    feat: create verifySyntax middleware

---
+++ server/middlewares/verifySyntax.js
@@ -0,0 +1,23 @@
+const verifySyntax = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let cleanEmail = false;
+  let cleanPassword = false;
+
+  if (email !== "") {
+    const validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    cleanEmail = validEmailRegex.test(email);
+  }
+  if (password !== "") {
+    const strongPasswordRegex = /^(?=.*\d)(?=.*[A-Z]).{7,}$/;
+    cleanPassword = strongPasswordRegex.test(password);
+  }
+  if (cleanEmail && cleanPassword) {
+    next();
+  } else {
+    res.status(400).send("Hacking");
+  }
+};
+
+export default verifySyntax;
