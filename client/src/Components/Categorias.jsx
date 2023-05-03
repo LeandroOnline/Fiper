922fe6556ff9dea2ab9700f0016cafdd786a2a55
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-05-03 11:18:44 -0300

    feat: add useSanitize to Categorias

--- client/src/Components/Categorias.jsx
+++ client/src/Components/Categorias.jsx
@@ -1,15 +1,27 @@
-
+import { useState } from "react";
 import "./Categorias.css";
+import useSanitize from "../hooks/useSanitize";
 
 const Categorias = () => {
+  const [detalleValue, setDetalleValue] = useState('')
+
+const detalle = (e)=>{
+  const sanitize= useSanitize(e.target.value)
+  setDetalleValue(sanitize)
+}
+
   return (
     <>
       <select id="entrada" name="tipo">
-        <option value="Ingresos" className="options">Ingresos</option>
-        <option value="Egresos" className="options">Egresos</option>
+        <option value="Ingresos" className="options">
+          Ingresos
+        </option>
+        <option value="Egresos" className="options">
+          Egresos
+        </option>
       </select>
-      <input placeholder="$" type="number" name="input" autoFocus />
-      <input placeholder="Detalle" name="detalle" />
+      <input placeholder="$" type="number" name="input" autoFocus/>
+      <input placeholder="Detalle" name="detalle" value={detalleValue} onChange={(e)=>detalle(e)}/>
     </>
   );
 };
