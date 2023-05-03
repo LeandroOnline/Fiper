d4d842625c9dd65339271ad2dadfa6a78aa53145
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-05-03 10:18:33 -0300

    feat: update sign p order

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import useVerify from "../hooks/useVerify";
 
 const API = "http://localhost:3000/api";
 
@@ -8,33 +9,47 @@ const SignUp = () => {
 
   const Sign = async (e) => {
     e.preventDefault();
-    await axios
-      .post(API + "/adduser", {
-        email: e.target.email.value,
-        password: e.target.password.value,
-      })
-      .then((data) => {
-        if (data.data === "Usuario existente") {
-          window.alert(
-            "Usuario 'Existente', prueve otro email o inicie sesion"
-          );
-        } else {
-          window.alert("Usuario Agregado");
-          navigate("/login");
-        }
-      })
-      .catch((err) => console.log(err));
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const verify = useVerify(email, password);
+
+    if (verify) {
+      await axios
+        .post(API + "/adduser", {
+          email: email,
+          password: password,
+        })
+        .then((data) => {
+          if (data.data === "Usuario existente") {
+            window.alert(
+              "Usuario 'Existente', prueve otro email o inicie sesion"
+            );
+          } else {
+            window.alert("Usuario Agregado");
+            navigate("/login");
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      window.alert("Email o contraseña invalidos");
+    }
   };
 
   return (
     <div>
+      <p>Su contraseña debe contener como minimo: </p>
+      <p>- Siete caracteres</p>
+      <p>- Una letra mayuscula</p>
+      <p>- Un numero</p>
       <form onSubmit={(e) => Sign(e)}>
-        <input placeholder="email" name="email" required/>
-        <input placeholder="contraseña" name="password" type="password" required/>
-        <p>Su contraseña debe contener como minimo: </p><br />
-        <p>- Siete caracteres</p>
-        <p>- Una letra mayuscula</p>
-        <p>- Un numero</p>
+        <input placeholder="email" name="email" required />
+        <input
+          placeholder="contraseña"
+          name="password"
+          type="password"
+          required
+        />
         <button type="submit">Registrarse</button>
       </form>
     </div>
