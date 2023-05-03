d8050362747d23f286c1db48f8dd40ad8535b177
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-05-03 10:13:32 -0300

    feat: add useVerify file

--- client/src/hooks/useVerify.js
+++ client/src/hooks/useVerify.js
@@ -1,12 +1,19 @@
-// const useVerify = (email = "", password = "") => {
-//   let cleanEmail = false;
-//   let cleanPassword = false;
-//   if (email !== "") {
-//     const validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-//     return validEmailRegex.test(email);
-//   }
-//   if (password !== "") {
-//   }
-// };
+const useVerify = (email = "", password = "") => {
+  let cleanEmail = false;
+  let cleanPassword = false;
+  if (email !== "") {
+    const validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    cleanEmail = validEmailRegex.test(email);
+  }
+  if (password !== "") {
+    const strongPasswordRegex = /^(?=.*\d)(?=.*[A-Z]).{8,}$/;
+    cleanPassword = strongPasswordRegex.test(password);
+  }
+  return cleanEmail && cleanPassword;
+};
 
-// export default useVerify;
+export default useVerify;
+
+
+
+// useVerify devuelve true cuando el email y el password son correctos a la vez
