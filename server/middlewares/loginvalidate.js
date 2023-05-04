f3223043927c37cc98bfc6b8a0bf77b3d572cd28
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-05-04 18:22:05 -0300

    feat: update loginvalidate middleware

--- server/middlewares/loginvalidate.js
+++ server/middlewares/loginvalidate.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const loginvalidate = async (req, res, next) => {
-  const token = await req.cookies.user;
 
-  if (token) {
-    const { id } = jwt.verify(req.cookies.user, process.env.SECRET_KEY);
+  const { id } = jwt.verify(req.cookies.user, process.env.SECRET_KEY);
+
+  if (id) {
     next();
   } else {
     res.status(500).send("No Token - loginvalidate");
   }
+  
 };
 
 module.exports = loginvalidate;
