1aefb55c0dabcb04c6b103a203e48fe1f7733612
LeandroOnline <leandrosavat@gmail.com>
2023-05-12 10:21:07 -0300

    feat: separe area config from component

--- client/src/Components/AreaGraph.jsx
+++ client/src/Components/AreaGraph.jsx
@@ -1,22 +1,16 @@
-import { Area } from "@ant-design/plots";
 import "./AreaGraph.css";
+import { Area } from "@ant-design/plots";
 import useGlobalStore from "../store/Store";
 import areaDataFormat from "../helpers/areaDataFormat";
+import configAreaGraph from "../utils/configAreaGraph";
 
 const AreaGraph = () => {
   const { inputs } = useGlobalStore();
   const data = areaDataFormat(inputs);
-  const config = {
-    data,
-    xField: "timePeriod",
-    yField: "value",
-    xAxis: {
-      range: [0, 1],
-    },
-  };
+
   return (
     <div className="areacontainer">
-      <Area {...config} />
+      <Area {...configAreaGraph(data)} />
     </div>
   );
 };
---
+++ client/src/utils/configAreaGraph.js
@@ -0,0 +1,10 @@
+const config = (data) => ({
+  data,
+  xField: "timePeriod",
+  yField: "value",
+  xAxis: {
+    range: [0, 1],
+  },
+});
+
+export default config;
