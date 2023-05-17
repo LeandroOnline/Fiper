c98d7387824a7a9039fd0942ac281957dfacda5b
LeandroOnline <leandrosavat@gmail.com>
2023-05-17 08:43:30 -0300

    feat: delete acount

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = memo(() => {
               onClick={() => deleteUser()}
               className={menu ? "navbutton" : "hide"}
             >
-              Configuracion
+              Elim.Cuenta
             </div>
           </>
         ) : (
