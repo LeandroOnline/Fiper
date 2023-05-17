289f87ed0d0a3122aef1cedffd2dae34aaffc8c6
LeandroOnline <leandrosavat@gmail.com>
2023-05-17 12:07:28 -0300

    feat: add secure: true

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -66,7 +66,9 @@ controllers.login = async (req, res) => {
       if (passwordOk) {
         const payload = { id: found._id };
         const token = jwt.sign(payload, process.env.SECRET_KEY);
-        res.cookie("user", token, { maxAge: 86400000 }).send("Logged");
+        res
+          .cookie("user", token, { maxAge: 86400000, secure: true })
+          .send("Logged");
         // La cookie expira en 24hs
       } else {
         res.send("Incorrect pasword");
