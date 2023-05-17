a240c2b4b823e25cbff2aba8923cda6cfd89c111
LeandroOnline <leandrosavat@gmail.com>
2023-05-17 12:55:14 -0300

    feat: update cookie

--- client/src/store/Store.jsx
+++ client/src/store/Store.jsx
@@ -3,27 +3,26 @@ import Cookies from "js-cookie";
 import axiosGetAllInputs from "../api/axiosGetAllInputs";
 // import { persist } from "zustand/middleware";
 
-
 const useGlobalStore = create(
   // persist(
-    (set, get) => ({
-      reset: false,
-      login: Cookies.get("user"),
-      inputs: [],
-      profits: [],
-      losses: [],
-      netPerMonth: [],
-      filtered: false,
-      setFiltered: (values)=> set({filtered: values}),
-      setNetPerMonth: (net) => set({ netPerMonth: net }),
-      setProfitsAndLosses: (profit, loss) =>
-        set({ profits: profit, losses: loss }),
-      setReset: () => set({ reset: !get().reset }),
-      setLogged: () => set({ login: false }),
-      setLogin: () => set({ login: Cookies.get("user") }),
-      storeGetAllInputs: async () =>
-        await axiosGetAllInputs().then((data) => set({ inputs: data })),
-    })
+  (set, get) => ({
+    reset: false,
+    login: Cookies.get("user"),
+    inputs: [],
+    profits: [],
+    losses: [],
+    netPerMonth: [],
+    filtered: false,
+    setFiltered: (values) => set({ filtered: values }),
+    setNetPerMonth: (net) => set({ netPerMonth: net }),
+    setProfitsAndLosses: (profit, loss) =>
+      set({ profits: profit, losses: loss }),
+    setReset: () => set({ reset: !get().reset }),
+    setLogged: () => set({ login: false }),
+    setLogin: () => set({ login: Cookies.get("user") }),
+    storeGetAllInputs: async () =>
+      await axiosGetAllInputs().then((data) => set({ inputs: data })),
+  })
   //   ,
   //   {
   //     name: "myFipe",
@@ -32,4 +31,5 @@ const useGlobalStore = create(
   // )
 );
 
+console.log(Cookies.get("user"));
 export default useGlobalStore;
