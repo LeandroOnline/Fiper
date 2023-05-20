3775ee47a76f3877e52f7c92b8886c4080b6f7e8
LeandroOnline <leandrosavat@gmail.com>
2023-05-20 16:12:35 -0300

    feat: test

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 //midlewares
-// app.use(cors());
+
 app.use(
   cors({
     origin: "https://fipe.savat.ar",
@@ -14,14 +14,7 @@ app.use(
     credentials: true,
   })
 );
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://fipe.savat.ar');
-//   next();
-// });
+app.set("trust proxy", 1);
 
 app.use(cookieParser());
 app.use(express.json());
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -62,8 +62,7 @@ controllers.login = async (req, res) => {
           .cookie("user", token, {
             maxAge: 86400000,
             secure: true,
-            domain: "fipe.savat.ar", // pruebo con .savat.ar nomas
-            // sameSite: "none",
+            domain: ".savat.ar", // pruebo con .savat.ar nomas
             path: "/",
           })
           .send("Logged");
