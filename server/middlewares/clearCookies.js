04f6c3da73c120a28f0227a18ff9e725d8014f33
LeandroOnline <leandrosavat@gmail.com>
2023-05-21 11:23:20 -0300

    feat: update middleware

--- server/middlewares/clearCookies.js
+++ server/middlewares/clearCookies.js
@@ -1,10 +1,15 @@
-const clearCookies = async (req,res,next)=>{
-    const cookie = req.cookies.user;
-    if(cookie) {
-        res.clearCookie("user")
-    }else{
-        next();
-    }
-}
+const clearCookies = async (req, res, next) => {
+  const cookie = req.cookies.user;
+  if (cookie) {
+    process.env.NODE_ENV === "development"
+      ? res.clearCookie("user")
+      : res.clearCookie("user", {
+          domain: ".savat.ar",
+          path: "/",
+        });
+  } else {
+    next();
+  }
+};
 
 module.exports = clearCookies;
