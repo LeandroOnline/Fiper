9019dbdbe274e70401a2523afddcfa89a93103e3
LeandroOnline <leandrosavat@gmail.com>
2023-06-12 02:06:06 -0300

    feat: improve email message

--- server/controllers/sendEmail.js
+++ server/controllers/sendEmail.js
@@ -22,11 +22,11 @@ const sendEmail = async (req, res) => {
       to: userFounded.email,
       subject: "Verificar cuenta de FIPE",
       html: 
-        `<h1>Bienvenido a FIPE</h1>  
+        `<h1>✦ Bienvenido a FIPE ✦</h1>  
         <p>Para activar tu cuenta de FIPE, accede al siguiente enlace:</p>
-        <a href="${url}${userFounded.id}"><strong>Activar cuenta</strong></a>
+        <a href="${url}${userFounded.id}"><strong>→ Activar cuenta ✓</strong></a>
+        <h3>Gracias por unirte.</h3><br/>
         <p>Si has recibido este correo por error, por favor ignóralo.</p>
-        <h3>Gracias por unirte.</h3>
         `,
     };
     const transport = nodemailer.createTransport(config);
