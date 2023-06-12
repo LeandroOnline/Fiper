7e74b0081141c1e5883148345d3d606b7348c312
LeandroOnline <leandrosavat@gmail.com>
2023-06-12 14:10:01 -0300

    feat: visual note finish

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,9 +1,12 @@
 import Note from "./Note"
 import "./Notes.css"
+import add from "../assets/agregar.png"
 const Notes = () => {
   return (
     <div className="notesContainer">
+        <div className="addNote"><img className="addNoteImg" src={add} alt="add" /></div>
         <Note/>
+
     </div>
   )
 }
