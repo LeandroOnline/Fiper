77849bdff70061b933377ea543951c47738f2cec
LeandroOnline <leandrosavat@gmail.com>
2023-06-21 17:31:06 -0300

    feat: fix spaces in navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Router from "./router/Router";
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Header />
         <Router />
         <Footer />
       </BrowserRouter>
--- client/src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-import "./Header.css";
-
-const Header = () => {
-  return <div className="HeaderContainer"></div>;
-};
-export default Header;
