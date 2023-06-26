c366074ce108a8ae6933fbde6e6a36f3d629a2b3
LeandroOnline <leandrosavat@gmail.com>
2023-06-25 22:24:09 -0300

    feat: test responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Router from "./router/Router";
+import "./resetcss.css"
+
 
 function App() {
   return (
