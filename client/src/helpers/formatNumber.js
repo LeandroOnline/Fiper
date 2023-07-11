8ee023295adbeb42dd31b221b85be23b00c6d89f
LeandroOnline <leandrosavat@gmail.com>
2023-07-11 11:57:05 -0300

    feat: add toFixed(1) for more clarity

--- client/src/helpers/formatNumber.js
+++ client/src/helpers/formatNumber.js
@@ -2,7 +2,7 @@ export default function formatNumber(numero) {
   if (numero >= 1000000) {
     return (numero / 1000000).toFixed(1) + " M";
   } else if (numero >= 1000) {
-    return (numero / 1000).toFixed(0) + " m";
+    return (numero / 1000).toFixed(1) + " m";
   } else {
     return numero.toString();
   }
